bf6feccb3d6414acb707892b8b2303a0f118e159
David <dhahn123@gmail.com>
2020-01-29 15:06:13 -0800

    validating StoreStructure in reducers index.ts

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,13 @@
 import { combineReducers } from 'redux';
-
-export const reducers = combineReducers({
-    counter: () => 1
-})
+import { Todo } from '../actions';
+import { todosReducer } from './todos';
+// describe entire state of store
+export interface StoreState {
+    todos: Todo[]
+}
+// With StoreState TS now looks at the object that we are passing into combinedReducers
+export const reducers = combineReducers<StoreState>({
+    // TS makes sure that todosReducer returns a value of type array of Todos (validation)
+    // lines up what we want StoreState to be
+    todos:todosReducer
+});
