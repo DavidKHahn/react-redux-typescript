26dd6cbbb70c58939da8973105f6295ccaf66d68
David <dhahn123@gmail.com>
2020-01-28 21:20:30 -0800

    functional component (TS)

--- src/index.tsx
+++ src/index.tsx
@@ -5,38 +5,32 @@ interface AppProps {
     color?: string;
 }
 
-interface AppState {
-    counter: number;
-}
+const App = (props: AppProps): JSX.Element => {
+    return <div>{props.color}</div>;
+};
 
-class App extends React.Component<AppProps, AppState> {
-    // need to pick between using 'state' or 'constructor' with TS
-    // state = { number: 0 }
+// class App extends React.Component<AppProps> {
+//     // need to pick between using 'state' or 'constructor' with TS
+//     state = { counter: 0 };
 
-    constructor(props: AppProps){
-        super(props);
-        this.state
-        this.state = { counter: 0 };
-    }
+//     onIncrement = (): void => {
+//         this.setState({ counter: this.state.counter + 1 });
+//     };
 
-    onIncrement = (): void => {
-        this.setState({ counter: this.state.counter + 1 });
-    };
+//     onDecrement = (): void => {
+//         this.setState({ counter: this.state.counter - 1 });
+//     };
 
-    onDecrement = (): void => {
-        this.setState({ counter: this.state.counter - 1 });
-    };
-
-    render(){
-        return (
-            <div>
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Increment</button>
-                {this.state.counter}
-            </div>
-            )
-    }
-}
+//     render(){
+//         return (
+//             <div>
+//                 <button onClick={this.onIncrement}>Increment</button>
+//                 <button onClick={this.onDecrement}>Increment</button>
+//                 {this.state.counter}
+//             </div>
+//             )
+//     }
+// }
 
 ReactDOM.render(
     <App color="red" />,
