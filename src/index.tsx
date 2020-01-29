a00bbf2ca928afe65a7ab6c245c61643988e1d9a
David <dhahn123@gmail.com>
2020-01-28 21:59:17 -0800

    npm install -D @types/module-name: for modules returning error (mainly from types)

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
-    <App color="red" />
+       <App />
     </Provider>,
     document.querySelector('#root')
 );
