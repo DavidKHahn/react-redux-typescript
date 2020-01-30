752525d1e852784f5bda033adc2c46411cad6fb8
David <dhahn123@gmail.com>
2020-01-29 17:33:33 -0800

    fix type definition files, complete deleteTodo function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchTodos, Todo } from '../actions';
+import { deleteTodo, fetchTodos, Todo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
     todos: Todo[];
-    fetchTodos(): any;
+    // need to specify what redux-thunk is
+    fetchTodos: Function;
+    deleteTodo: typeof deleteTodo;
 }
 
 class _App extends React.Component<AppProps> {
     onButtonClick = (): void => {
         this.props.fetchTodos();
     };
+
+    onToDoClick = (id: number):void => {
+        this.props.deleteTodo(id);
+    }
     // helper method
     renderList(): JSX.Element[] {
         return this.props.todos.map((todo: Todo) => {
-            return <div key={todo.id}>{todo.title}</div>
-        })
-    }
+          return (
+              <div onClick={() => this.onToDoClick(todo.id)} key={todo.id}>
+                {todo.title}
+              </div>
+          );
+        });
+      }
 
     render(){
         return (
@@ -33,4 +43,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
     return { todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App)
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App)
